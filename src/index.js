b5765b1c06df07b14f74f5e9c87fdf0373e0e095
github username <github email address>
2020-07-01 19:02:24 -0400

    Add Toy Info to the Card

--- src/index.js
+++ src/index.js
@@ -1,15 +1,65 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+    toyTaleToggle();
+    makeCardsForToys();
+
 });
+
+function toyTaleToggle() {
+    const addBtn = document.querySelector("#new-toy-btn");
+    const toyFormContainer = document.querySelector(".container");
+    addBtn.addEventListener("click", () => {
+        // hide & seek with the form
+        addToy = !addToy;
+        if (addToy) {
+            toyFormContainer.style.display = "block";
+        } else {
+            toyFormContainer.style.display = "none";
+        }
+    });
+}
+
+function makeCardsForToys() {
+    const toyCardCollection = document.getElementById("toy-collection")
+    fetch("http://localhost:3000/toys")
+        .then((response) => response.json())
+        .then((toys) => {
+            toys.forEach((toy) => toyCardCollection.appendChild(makeAcard(toy)));
+        });
+}
+
+function makeAcard(toy) {
+    console.log(toy);
+    const divCard = document.createElement("div");
+    divCard.setAttribute("class", "card");
+
+    const h2ToyName = document.createElement("h2");
+    h2ToyName.textContent = toy.name;
+    divCard.appendChild(h2ToyName);
+
+    const toyImg = document.createElement("img");
+    toyImg.setAttribute("src", toy.image);
+    toyImg.setAttribute("class", "toy-avatar")
+    divCard.appendChild(toyImg);
+
+    const pLikes = document.createElement("p");
+    pLikes.textContent = toy.likes + " Likes";
+    divCard.appendChild(pLikes);
+
+    const likeButton = document.createElement("button");
+    likeButton.setAttribute("class", "like-btn");
+    likeButton.textContent = "Like";
+    divCard.appendChild(likeButton);
+
+
+
+
+
+
+
+    return divCard;
+    // <p>4 Likes </p>
+    // <button class="like-btn">Like <3</button>
+
+}
